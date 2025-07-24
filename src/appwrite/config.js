d6f638a7bae068f1708a4f06db89267bfd4f3b9a
Pavan-Kolhe <pavandigambarkolhe@gmail.com>
2025-07-24 10:53:16 +0530

    feat: implement authentication flow with Login and Signup components; add Protected route for authentication checks

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -7,7 +7,7 @@ import { Client, ID, Databases, Storage, Query } from "appwrite";
 export class Service {
   client = new Client();
   databases;
-  becket;
+  bucket;
 
   constructor() {
     this.client
@@ -15,7 +15,7 @@ export class Service {
       .setProject(conf.appwriteProjectId);
 
     this.databases = new Databases(this.client);
-    this.becket = new Storage(this.client);
+    this.bucket = new Storage(this.client);
   }
 
   async createPost({ title, slug, content, featuredImage, status, userId }) {
@@ -98,7 +98,7 @@ export class Service {
 
   async uploadFile(file) {
     try {
-      return await this.becket.createFile(
+      return await this.bucket.createFile(
         conf.appwriteBucketId,
         ID.unique(),
         file
@@ -111,7 +111,7 @@ export class Service {
 
   async deleteFile(fileId) {
     try {
-      await this.becket.deleteFile(conf.appwriteBucketId, fileId);
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
       return true;
     } catch (error) {
       console.log("Appwrite service :: deleteFile :: error", error);
@@ -120,7 +120,7 @@ export class Service {
   }
 
   getFilePreview(fileId) {
-    return this.becket.getFileView(conf.appwriteBucketId, fileId);
+    return this.bucket.getFileView(conf.appwriteBucketId, fileId);
   }
 }
 
